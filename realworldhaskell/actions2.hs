0f50868b48dc4db14a38878589d6141db621c7e3
Low-Wee Heng <alpha116@gmail.com>
2014-10-28 10:47:27 +0800

    Actions

---
+++ realworldhaskell/actions2.hs
@@ -0,0 +1,13 @@
+str2message :: String -> String
+str2message = (++) "Data: "
+
+str2action :: String -> IO ()
+str2action = putStrLn . str2message
+
+numbers :: [Int]
+numbers = [1..10]
+
+main = do
+  str2action "Start of the program"
+  mapM_ (str2action . show) numbers
+  str2action "Done!"
