c763e6a4d19104c90cb09d5d3e4bae911ec9e311
Low-Wee Heng <alpha116@gmail.com>
2014-10-29 15:53:34 +0800

    Done with Chapter 9

--- realworldhaskell/ControlledVisit.hs
+++ realworldhaskell/ControlledVisit.hs
@@ -1,9 +1,10 @@
 import Control.Monad (filterM, forM, liftM)
 import System.Directory (Permissions(..), getModificationTime, getPermissions, getDirectoryContents)
 import Data.Time.Clock (UTCTime(..))
-import System.FilePath (takeExtension, (</>))
+import System.FilePath (takeExtension, (</>), takeFileName)
 import Control.Exception (bracket, handle)
 import System.IO (IOMode(..), hClose, hFileSize, openFile)
+import Data.Char (toLower)
 
 import RecursiveContents (getRecursiveContents)
 
@@ -41,3 +42,45 @@ getInfo path = do
   size <- maybeIO (bracket (openFile path ReadMode) hClose hFileSize)
   modified <- maybeIO (getModificationTime path)
   return (Info path perms size modified)
+
+data Iterate seed = Done {unwrap :: seed}
+                  | Skip {unwrap :: seed}
+                  | Continue {unwrap :: seed}
+                    deriving (Show)
+
+type Iterator seed = seed -> Info -> Iterate seed
+
+foldTree :: Iterator a -> a -> FilePath -> IO a
+foldTree iter initSeed path = do
+  endSeed <- fold initSeed path
+  return (unwrap endSeed)
+  where
+    fold seed subpath = getUsefulContents subpath >>= walk seed
+    walk seed (name : names) = do
+      let path' = path </> name
+      info <- getInfo path'
+      case iter seed info of
+        done@(Done _) -> return done
+        Skip seed' -> walk seed' names
+        Continue seed'
+          | isDirectory info -> do
+              next <- fold seed' path'
+              case next of
+                done@(Done _) -> return done
+                seed'' -> walk (unwrap seed'') names
+          | otherwise -> walk seed' names
+    walk seed _ = return (Continue seed)
+
+atMostThreePictures :: Iterator [FilePath]
+atMostThreePictures paths info
+  | length paths == 3 = Done paths
+  | isDirectory info && takeFileName path == ".svn" = Skip paths
+  | extension `elem` [".jpg", ".png"] = Continue (path : paths)
+  | otherwise = Continue paths
+  where extension = map toLower (takeExtension path)
+        path = infoPath info
+
+countDirectories count info =
+  Continue (if isDirectory info
+            then count + 1
+            else count)
