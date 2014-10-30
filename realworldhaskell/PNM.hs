71f155ef1b00545ae6d05797fc3a149db7ad1ccb
Low-Wee Heng <alpha116@gmail.com>
2014-10-30 12:51:57 +0800

    Chaining parsers together

--- realworldhaskell/PNM.hs
+++ realworldhaskell/PNM.hs
@@ -1,6 +1,8 @@
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
 import Data.Char (isSpace)
+import Data.Int
+import Data.Word
 
 data Greymap = Greymap {
   greyWidth :: Int,
@@ -76,3 +78,58 @@ parseP5_take2 s =
 
 skipSpace :: (a, L.ByteString) -> Maybe (a, L.ByteString)
 skipSpace (a, s) = Just (a, L8.dropWhile isSpace s)
+
+data ParseState = ParseState {
+  string :: L.ByteString,
+  offset :: Int64
+} deriving (Show)
+
+simpleParse :: ParseState -> (a, ParseState)
+simpleParse = undefined
+
+betterParse :: ParseState -> Either String (a, ParseState)
+betterParse = undefined
+
+newtype Parse a = Parse {
+  runParse :: ParseState -> Either String (a, ParseState)
+}
+
+identity :: a -> Parse a
+identity a = Parse (\s -> Right (a, s))
+
+parse :: Parse a -> L.ByteString -> Either String a
+parse parser initState = case runParse parser (ParseState initState 0) of
+                           Left err -> Left err
+                           Right (result, _) -> Right result
+
+modifyOffset :: ParseState -> Int64 -> ParseState
+modifyOffset initState newOffset = initState { offset = newOffset }
+
+parseByte :: Parse Word8
+parseByte = 
+  getState ==> \initState ->
+  case L.uncons (string initState) of
+    Nothing -> bail "no more input"
+    Just (byte, remainder) ->
+        putState newState ==> \_ ->
+        identity byte
+      where newState = initState { string = remainder, offset = newOffset }
+            newOffset = offset initState + 1
+
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right ((), s))
+
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $
+           "byte offset " ++ show (offset s) ++ ": " ++ err
+
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser = Parse chainedParser
+  where chainedParser initState = 
+          case runParse firstParser initState of
+            Left errMessage -> Left errMessage
+            Right (firstResult, newState) ->
+              runParse (secondParser firstResult) newState
