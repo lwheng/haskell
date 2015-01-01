d3f6c637a7209e10d0186a73615c437d51245185
Low-Wee Heng <alpha116@gmail.com>
2015-01-01 20:06:36 +0800

    Add views to templates

--- yosog/Handler/Echo.hs
+++ yosog/Handler/Echo.hs
@@ -3,4 +3,5 @@ module Handler.Echo where
 import Import
 
 getEchoR :: Text -> Handler Html
-getEchoR theText = defaultLayout [whamlet|<h1>#{theText}|]
+-- getEchoR theText = defaultLayout [whamlet|<h1>#{theText}|]
+getEchoR theText = defaultLayout $(widgetFile "echo")
