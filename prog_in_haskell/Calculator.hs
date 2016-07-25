b9ac76d4cea0e2edaec391c69d2d445cdfe70e55
Low-Wee Heng <alpha116@gmail.com>
2016-07-25 23:36:42 +0800

    WIP

--- prog_in_haskell/Calculator.hs
+++ prog_in_haskell/Calculator.hs
@@ -33,3 +33,36 @@ seqn []     = return ()
 seqn (a:as) = do
                 a
                 seqn as
+
+box :: [String]
+box = [
+        "+---------------+"
+      , "|               |"
+      , "+---+---+---+---+"
+      , "| q | c | d | = |"
+      , "+---+---+---+---+"
+      , "| 1 | 2 | 3 | + |"
+      , "+---+---+---+---+"
+      , "| 4 | 5 | 6 | - |"
+      , "+---+---+---+---+"
+      , "| 7 | 8 | 9 | * |"
+      , "+---+---+---+---+"
+      , "| 0 | ( | ) | / |"
+      , "+---+---+---+---+"
+      ]
+
+buttons :: [Char]
+buttons = standard ++ extra
+  where
+    standard = "qcd=123+456-789*0()/"
+    extra    = "QCD \ESC\BS\DEL\n"
+
+showbox :: IO ()
+showbox = seqn [writeat (1, y) xs | (y, xs) <- zip [1..] box]
+
+display
+  :: String
+  -> IO ()
+display xs = do
+               writeat (3, 2) "                 "
+               writeat (3, 2) (reverse (take 13 (reverse xs)))
