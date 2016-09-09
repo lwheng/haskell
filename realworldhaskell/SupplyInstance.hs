1c19d4672eba70181df197f1450e43654fb5fb41
Low-Wee Heng <alpha116@gmail.com>
2016-09-09 19:17:21 +0800

    instances for Reader

--- realworldhaskell/SupplyInstance.hs
+++ realworldhaskell/SupplyInstance.hs
@@ -1,9 +1,21 @@
+module SupplyInstance (
+    Reader
+  , runReader
+) where
+
+import Control.Monad
+
 newtype Reader e a = R {
                          runReader :: e -> a
                        }
 
 instance Functor (Reader e) where
-  fmap = undefined
--- instance Monad (Reader e) where
---   return a = R $ \_ -> a
---   m >>= k  = R $ \r -> runReader (k (runReader m r)) r
+  fmap f r = R $ \e -> f (runReader r e)
+
+instance Applicative (Reader e) where
+  pure a  = R $ \_ -> a
+  x <*> y = R $ \e -> (runReader x e) (runReader y e)
+
+instance Monad (Reader e) where
+  return a = R $ \_ -> a
+  m >>= k  = R $ \r -> runReader (k (runReader m r)) r
